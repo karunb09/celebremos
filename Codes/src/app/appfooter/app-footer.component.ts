e94f539aa8fdb653bbe96eb3e27ae462c9a6e98b
Hari Priya Jupally <jharipriya@outlook.com>
2019-06-25 11:42:20 -0500

    Commit footer component

---
+++ Codes/src/app/appfooter/app-footer.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic toolbar
+ */
+@Component({
+  selector: 'app-header',
+  templateUrl: './app-header.component.html',
+})
+export class AppHeader {}
