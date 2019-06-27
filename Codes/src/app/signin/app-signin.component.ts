a106d42c652ccccdf7b91e38b16558f55c628f67
reddypan <S533574@nwmissouri.edu>
2019-06-27 11:13:28 -0500

    Added contents to register/register.component.ts

--- Codes/src/app/signin/app-signin.component.ts
+++ Codes/src/app/signin/app-signin.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 //import { AuthenticationService, TokenPayload } from '../authentication.service';
-=======
->>>>>>> 4661cf3b30ee59c40a0bbfd31d96acd7fa273824
 import { Router } from '@angular/router';
 
 /**
@@ -15,24 +12,6 @@ import { Router } from '@angular/router';
 })
 export class SignINComponent {}
 
-<<<<<<< HEAD
-// export class LoginComponent {
-//   credentials: TokenPayload = {
-//     email: '',
-//     password: ''
-//   };
-
-//   constructor(private auth: AuthenticationService, private router: Router) {}
-
-//   login() {
-//     this.auth.login(this.credentials).subscribe(() => {
-//       this.router.navigateByUrl('/profile');
-//     }, (err) => {
-//       console.error(err);
-//     }); 
-//   }
-// }
-=======
   // credentials: TokenPayload = {
   //   email: '',
   //   password: ''
@@ -46,4 +25,3 @@ export class SignINComponent {}
   //   }, (err) => {
   //     console.error(err);
 
->>>>>>> 4661cf3b30ee59c40a0bbfd31d96acd7fa273824
