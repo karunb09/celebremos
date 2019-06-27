064bfafacd24b6ba7c10410f77fe4d0765190f26
karun <11141770+karunb09@users.noreply.github.com>
2019-06-26 21:17:10 -0500

    removed redundancy

--- Codes/src/app/register/register.component.ts
+++ Codes/src/app/register/register.component.ts
@@ -1,33 +1,10 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-//import { AuthenticationService, TokenPayload } from '../authentication.service';
-=======
->>>>>>> 4661cf3b30ee59c40a0bbfd31d96acd7fa273824
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html'
 })
-<<<<<<< HEAD
- export class RegisterComponent {
-//   credentials: TokenPayload = {
-//     email: '',
-//     name: '',
-//     password: ''
-   };
-
-//   constructor(private auth: AuthenticationService, private router: Router) {}
-
-//   register() {
-//     this.auth.register(this.credentials).subscribe(() => {
-//       this.router.navigateByUrl('/home-page');
-//     }, (err) => {
-//       console.error(err);
-//     });
-//   }
-// }
-=======
 export class RegisterComponent {
   // credentials: TokenPayload = {
   //   email: '',
@@ -45,4 +22,3 @@ export class RegisterComponent {
   //   });
   // }
 }
->>>>>>> 4661cf3b30ee59c40a0bbfd31d96acd7fa273824
