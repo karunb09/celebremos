a042ebf830eec7d219fef60c259ba5a3657c09e9
saikumar <sainalivela@gmail.com>
2019-07-02 12:09:01 -0500

    edited register page

--- Codes/src/app/register/register.component.ts
+++ Codes/src/app/register/register.component.ts
@@ -25,14 +25,37 @@ export class RegisterComponent implements OnInit, OnDestroy {
     });
   }
 
-  onSignUp(form: NgForm) {
-    if (form.invalid) {
-      return;
+//   function checkEmail(theForm) {
+//     if (theForm.EMAIL_1.value != theForm.EMAIL_2.value)
+//     {
+//         alert('Those emails don\'t match!');
+//         return false;
+//     } else {
+//         return true;
+//     }
+// }
+
+checkEmail(form: NgForm){
+  if (form.value.password != form.value.password1)
+    {
+        alert('Passwords do not match! Please check and enter same passwords');
+        return false;
+    } else {
+        return true;
     }
-// tslint:disable-next-line: max-line-length
-    this.isLoading = true;
-    this.authService.createUser( form.value.firstname, form.value.lastname, form.value.username, form.value.email, form.value.password, form.value.phone);
-  }
+}
+
+  onSignUp(form: NgForm) {
+    if(this.checkEmail(form)){
+      if (form.invalid) {
+        return;
+      }
+  // tslint:disable-next-line: max-line-length
+      this.isLoading = true;
+      this.authService.createUser( form.value.firstname, form.value.lastname, form.value.username, form.value.email, form.value.password, form.value.phone);
+    }
+
+    }
 
   ngOnDestroy() {
     this.authStatusSub.unsubscribe();
