fd2133950768b81564da4e7c5c6b5d330beb5e1e
PoojithaSingam <42949737+PoojithaSingam@users.noreply.github.com>
2019-09-29 22:53:34 -0500

    Added and made changes to routing and service.ts files

--- Codes/src/app/app-routing.module.ts
+++ Codes/src/app/app-routing.module.ts
@@ -7,7 +7,9 @@ import { PostListComponent } from './posts/post-list/post-list.component';
 import { AuthGuard } from './auth.guard';
 import { NewPasswordComponent } from './newpassword/newpassword.component';
 import { ResetPasswordComponent } from './newpassword/resetpassword/resetpassword.component';
-import { CsvReadComponent } from './csvread/app-csvread.component';
+import { AddGuestsComponent } from './add guests/addguests.component';
+import { ActivateUserComponent } from './newpassword/activateuser/activateuser.component';
+import { RSVPComponent } from './rsvp/rsvp.component';
 
 // Setting path to redirect next page
 
@@ -19,7 +21,9 @@ const routes: Routes =  [
   { path: 'list', component: PostListComponent, canActivate: [AuthGuard] },
   { path: 'reset', component: NewPasswordComponent},
   { path: 'reset/:userId', component: ResetPasswordComponent},
-  { path: 'csvupload', component: CsvReadComponent}
+  { path: 'addGuests/:eventName', component: AddGuestsComponent},
+  { path: 'activateUser/:userId', component: ActivateUserComponent},
+  { path: 'rsvp/:postId/:emailId', component: RSVPComponent},
 ];
 
 
