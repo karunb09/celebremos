bee1f028ab96f51c977d7af1a20e53c27c1ec69e
sairam <sairambadisa9@gmail.com>
2019-11-10 22:23:38 -0600

    committed the code of the cards

---
+++ Codes/src/app/cards/party-cards/party-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-party-cards',
+  templateUrl: './party-cards.component.html',
+  styleUrls: ['./party-cards.component.css']
+})
+export class PartyCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
