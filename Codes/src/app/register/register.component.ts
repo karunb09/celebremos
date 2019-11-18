4ea49eb6abe6e7888b000834d7f079b26d72edae
saikumar <sainalivela@gmail.com>
2019-11-17 21:52:53 -0600

    register component pushed

--- Codes/src/app/register/register.component.ts
+++ Codes/src/app/register/register.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
-
 import { AuthService } from '../auth.service';
 import { Subscription } from 'rxjs';
 
@@ -10,14 +8,13 @@ import { Subscription } from 'rxjs';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit, OnDestroy {
 
-
   isLoading = false;
-
   private authStatusSub: Subscription;
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService) {}
 
   ngOnInit() {
     this.authStatusSub = this.authService.getAuthStatusListener().subscribe(authStatus => {
@@ -39,12 +36,10 @@ export class RegisterComponent implements OnInit, OnDestroy {
       if (form.invalid) {
         return;
       }
-      // tslint:disable-next-line: max-line-length
       this.isLoading = true;
       this.authService.createUser(form.value.firstname, form.value.lastname,
         form.value.username, form.value.email, form.value.password, form.value.phone);
     }
-
   }
 
   ngOnDestroy() {
